df303ad3ed98cce936c63233f1228a7cdd85b87b
neaimt <pas120344@gmail.com>
2024-02-16 12:45:58 +0900

    View reorganize

---
+++ Foodmate/Views/FoodmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodmateApp.swift
+//  Foodmate
+//
+//  Created by 박근경 on 2024/01/08.
+//
+
+import SwiftUI
+
+@main
+struct FoodmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
